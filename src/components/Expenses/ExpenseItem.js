3ae5074aa6be9a28e243eca4f9b4b0608f8de23c
Meghna <meghna@stackroute.in>
2021-07-21 19:47:36 +0530

    useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js'
+import React, { useState } from 'react';
+// useState is a function provided by React library
+// - allows us to define values as state where changes to these values should reflect 
+// in the component function being called again
+// useState is a React hook
+// all hooke start with the word use
 
 const ExpenseItem = (props) => {
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);
+
+    // let title = props.title;
 
     const clickHandler = () => {
-        title = 'updated!!!';
+        // title = 'updated!!!';
+        setTitle("Updated Title");
         console.log(title);
     }
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
