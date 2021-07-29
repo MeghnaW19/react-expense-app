0c99077ef55618570c1fe5fa5ebee6e016e61577
Meghna <Meghna@stackroute.in>
2021-07-29 16:30:56 +0530

    2nd way conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,3 @@
-
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -13,57 +12,24 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
-  });
-  
+  })
 
-  import ExpenseItem from './ExpenseItem';
-  import Card from '../UI/Card';
-  import './Expenses.css';
-  import ExpensesFilter from './ExpenseFilter';
-  import React, { useState, usestate } from 'react';
-  
-  const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
-    const filterChangeHandler = (selectedYear) => {
-      setFilteredYear(selectedYear);
-    }
-  
-    const filteredExpenses = props.items.filter(expense => {
-      return expense.date.getFullYear().toString() === filteredYear;
-    })
-  
-    return (
-      <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map((expense) =>
-  
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date} />)
-        }
-  
-      </Card>
-    );
+  let expenseContent = <p>No expenses found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date} />));
   }
-  
-  export default Expenses;
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {
-        filteredExpenses.length === 0 ? (<p>No expenses found!</p>) :
-        filteredExpenses.map((expense) =>
 
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date} />)
-      }
-    
-
+      {expenseContent4}
     </Card>
   );
 }
