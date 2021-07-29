0838f7e76e2d75a8e1d09c822a9e5d3da43cd368
Meghna <Meghna@stackroute.in>
2021-07-29 16:25:12 +0530

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,39 @@ const Expenses = (props) => {
   });
   
 
+  import ExpenseItem from './ExpenseItem';
+  import Card from '../UI/Card';
+  import './Expenses.css';
+  import ExpensesFilter from './ExpenseFilter';
+  import React, { useState, usestate } from 'react';
+  
+  const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+    }
+  
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    })
+  
+    return (
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {filteredExpenses.map((expense) =>
+  
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} />)
+        }
+  
+      </Card>
+    );
+  }
+  
+  export default Expenses;
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
