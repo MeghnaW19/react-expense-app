5be2640f48e4bce0e277b5e40f116ba0d6ffe794
Meghna <Meghna@stackroute.in>
2021-08-10 12:00:40 +0530

    conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpenseFilter';
 import React, { useState, usestate } from 'react';
-
+import ExpensesList from './ExpensesList';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = (selectedYear) => {
@@ -14,22 +13,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
-  let expenseContent = <p>No expenses found!</p>;
 
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date} />));
-  }
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-      {expenseContent4}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 }
